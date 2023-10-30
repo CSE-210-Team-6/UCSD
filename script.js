197eaaea10117f7b07a45ac4e2fde614c286f3f2
Kashish Jain <kashish.jain@spit.ac.in>
2023-10-30 11:21:55 -0700

    Fixed weird rendering issue

--- script.js
+++ script.js
@@ -37,16 +37,9 @@ function func4() {
   });
 }
 
-function func5() {
+function matchHeights() {
+
   $(".panel-body").matchHeight();
   $(".panel-news-title").matchHeight();
-  _linkedin_data_partner_id = "46649";
-  (function () {
-    var s = document.getElementsByTagName("script")[0];
-    var b = document.createElement("script");
-    b.type = "text/javascript";
-    b.async = true;
-    b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
-    s.parentNode.insertBefore(b, s);
-  })();
+
 }

883a844bd839154ad4007691046470a32116ca38
smruthig <smruthig01@gmail.com>
2023-10-30 17:49:37 -0700

    passive scrolling fix

--- script.js
+++ script.js
@@ -37,3 +37,15 @@ function matchHeights() {
   $(".panel-news-title").matchHeight();
 
 }
+
+// Passive event listeners
+jQuery.event.special.touchstart = {
+  setup: function( _, ns, handle ) {
+      this.addEventListener("touchstart", handle, { passive: !ns.includes("noPreventDefault") });
+  }
+};
+jQuery.event.special.touchmove = {
+  setup: function( _, ns, handle ) {
+      this.addEventListener("touchmove", handle, { passive: !ns.includes("noPreventDefault") });
+  }
+};
